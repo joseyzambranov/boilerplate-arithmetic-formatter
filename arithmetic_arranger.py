60b0d61d236b19468c115e503cf8972db1e50a98
joseyzambranov <joseyzambranovpe@gmail.com>
2023-10-29 19:32:22 -0500

    finist

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,79 +1,54 @@
 import re
-def arithmetic_arranger(problems):
-    print(problems)
+
+def arithmetic_arranger(problems, show_answers=False):
     arranged_problems = ''
     first_row = ''
     second_row = ''
     third_row = ''
+    answers = []  # Lista para almacenar las respuestas
+
+    if len(problems) > 5:
+        return "Error: Too many problems."
+
     for problem in problems:
-        first_row = problem.split(" ")[0]
-        second_row = problem.split(" ")[1]
-        third_row = problem.split(" ")[2]
-    arranged_problems = first_row + '\n' + second_row + third_row    
+        parts = problem.split()
+
+        if len(parts) != 3:
+            return "Error: Cada problema debe contener exactamente dos operandos y un operador."
+
+        operand1, operator, operand2 = parts
+
+        if not re.match(r'^\d+$', operand1) or not re.match(r'^\d+$', operand2):
+            return "Error: Numbers must only contain digits."
+
+        if operator not in "+-":
+            return "Error: Operator must be '+' or '-'."
+
+        if len(str(operand1)) > 4 or len(str(operand2)) > 4:
+            return "Error: Numbers cannot be more than four digits."
+
+        operand1 = int(operand1)  # Convierte operand1 a entero
+        operand2 = int(operand2)  # Convierte operand2 a entero
+
+        if operator == "+":
+            result = operand1 + operand2
+        else:
+            result = operand1 - operand2
+
+        max_len = max(len(str(operand1)), len(str(operand2)))
+        first_row += str(operand1).rjust(max_len + 2)
+        second_row += operator + str(operand2).rjust(max_len + 1)
+        third_row += '-' * (max_len + 2)
+        answers.append(str(result).rjust(max_len + 2))  # Agrega la respuesta
+
+        if problem != problems[-1]:
+            first_row += '    '
+            second_row += '    '
+            third_row += '    '
+
+    arranged_problems = '\n'.join([first_row, second_row, third_row])
+    
+    if show_answers:
+        arranged_problems += '\n' + '    '.join(answers)  # Agregar respuestas si show_answers es True
+    
     return arranged_problems
-
-
-#import re
-
-#
-#def arithmetic_arranger(problems, solve = False):
-#    if(len(problems) > 5):
-#      return "Error: Too many problems."
-#  # this
-#    # ["3 + 855", "3801 - 2", "45 + 43", "123 + 49"]
-#    first= ""
-#    second=""
-#    lines= ""
-#    sumx=""
-#    string = ""
-#    for problem in problems: 
-#      if(re.search("[^\s0-9.+-]", problem)):
-#        if(re.search("[/]", problem) or re.search("[*]", problem)):
-#          return "Error: Operator must be '+' or '-'."
-#        return "Error: Numbers must only contain digits."
-#      firstNumber = problem.split(" ")[0]
-#      operator = problem.split(" ")[1]
-#      secondNumber = problem.split(" ")[2]
-#
-#      if(len(firstNumber) >= 5 or len(secondNumber) >= 5):
-#        return "Error: Numbers cannot be more than four digits."
-#
-#      sum = ""
-#      if(operator == "+"):
-#        sum = str(int(firstNumber) + int(secondNumber))
-#      elif(operator == "-"):
-#        sum = str(int(firstNumber) - int(secondNumber))
-#
-#      length = max(len(firstNumber), len(secondNumber)) + 2
-#      top = str(firstNumber).rjust(length)
-#      bottom = operator + str(secondNumber).rjust(length - 1)
-#      line = ''
-#      res = str(sum).rjust(length)
-#      for s in range(length):
-#        line += '-'
-#      
-#     
-#      if problem != problems[-1]:
-#        first += top + '    '
-#        second += bottom + '    '
-#        lines += line + '    '
-#        sumx += res + '    '
-#      else:
-#        first += top
-#        second += bottom
-#        lines += line
-#        sumx += res
-#
-#
-#
-#    if solve:
-#        string = first + '\n' + second + '\n' + lines + '\n' + sumx
-#    else:
-#        string = first + '\n' + second + '\n' + lines
-#    return string
-#
-#   
-#  # into this
-#    # "    3      3801      45      123\n+ 855    -    2    + 43    +  49\n-----    ------    ----    -----"
-#
-#    
